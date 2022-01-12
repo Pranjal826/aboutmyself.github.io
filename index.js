d19aa718d004fab52e1bc1f3c1f794c3f3c6a6ad
Pranjal826 <90145718+Pranjal826@users.noreply.github.com>
2022-01-12 20:04:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let menu = document.querySelector('#menu-bars');
 let header = document.querySelector('header');
 
 menu.onclick = () =>{
-    menu.classList.toggle('');
+    menu.classList.toggle('fa-times');
     header.classList.toggle('active');
 }
 
